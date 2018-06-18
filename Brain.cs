bf346e6284dd28a323f2f587d5e1459377aa0aaa
venkatesh <venkateshtata9@gmail.com>
2018-06-18 05:47:54 +0530

    Add files via upload

---
+++ Brain.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.ThirdPerson;
+
+[RequireComponent(typeof(ThirdPersonCharacter))]
+public class Brain : MonoBehaviour
+{
+    public int DNALength = 1;
+    public float timeAlive;
+    public DNA dna;
+    public float distanceTravelled;
+    Vector3 startPosition;
+
+    private ThirdPersonCharacter m_Character;
+    private Vector3 m_Move;
+    private bool m_jump;
+    bool alive = true;
+
+    void OnCollisionEnter(Collision obj)
+    {
+        if (obj.gameObject.tag == "dead")
+        {
+            alive = false;
+        }
+    }
+
+    public void Init()
+    {
+        //Initialize DNA
+        //0 forward
+        //1 back
+        //2 left
+        //3 right
+        //4 jump
+        //5 crouch
+        dna = new DNA(DNALength, 6);
+        m_Character = GetComponent<ThirdPersonCharacter>();
+        timeAlive = 0;
+        alive = true;
+    }
+    void Update()
+    {
+
+    }
+    private void FixedUpdate()
+    {
+        //read DNA
+        float h = 0;
+        float v = 0;
+        bool crouch = false;
+        if (dna.GetGene(0) == 0) v = 1;
+        else if (dna.GetGene(0) == 1) v = -1;
+        else if (dna.GetGene(0) == 2) h = -1;
+        else if (dna.GetGene(0) == 3) h = 1;
+        else if (dna.GetGene(0) == 4) m_jump = true;
+        else if (dna.GetGene(0) == 5) crouch = true;
+
+        m_Move = v * Vector3.forward + h * Vector3.right;
+        m_Character.Move(m_Move, crouch, m_jump);
+        m_jump = false;
+        if (alive) { 
+        timeAlive += Time.deltaTime;
+        distanceTravelled = Vector3.Distance(this.transform.position, startPosition);
+        }
+
+    }
+}
---
+++ DNA.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DNA
+{
+    List<int> genes = new List<int>();
+    int dnaLength = 0;
+    int maxValues = 0;
+
+    public DNA(int l, int v)
+    {
+        dnaLength = l;
+        maxValues = v;
+        SetRandom();
+    }
+    public void SetRandom()
+    {
+        genes.Clear();
+        for (int i = 0; i < dnaLength; i++)
+        {
+            genes.Add(Random.Range(0, maxValues));
+        }
+    }
+    public void SetInt(int pos, int value)
+    {
+        genes[pos] = value;
+    }
+    public void Combine(DNA d1, DNA d2)
+    {
+        for (int i = 0; i < dnaLength / 2.0; i++)
+        {
+            if (i < dnaLength / 2.0)
+            {
+                int c = d1.genes[i];
+                genes[i] = c;
+            }
+            else
+            {
+                int c = d2.genes[i];
+                genes[i] = c;
+            }
+        }
+    }
+    public void Mutate()
+    {
+        genes[Random.Range(0, dnaLength)] = Random.Range(0, dnaLength);
+    }
+    public int GetGene(int pos)
+    {
+        return genes[pos];
+    }
+}
